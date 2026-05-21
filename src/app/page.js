5effa936cf1a4dcdf09cf454b9f380937cf66c62
Mehedy Hasan <110835185+mehedyPUST@users.noreply.github.com>
2026-05-21 22:22:14 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Banner from "@/components/Banner";
 import Image from "next/image";
 import HowItWorks from '@/components/HowItWorks';
+import WhyChooseUs from '@/components/WhyChooseUs';
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <HowItWorks />
+      <WhyChooseUs />
 
     </div>
   );
