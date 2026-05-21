0b8a46aeebb73c86c617cb4dcc206bba73bccb02
Mehedy Hasan <110835185+mehedyPUST@users.noreply.github.com>
2026-05-21 22:16:25 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
 import Image from "next/image";
+import HowItWorks from '@/components/HowItWorks';
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <HowItWorks />
 
     </div>
   );
